ed467f1aa38484e29ca7ec70f5992dda7bfd967d
balajihack21 <balajiwebhost@gmail.com>
2024-01-07 12:51:50 +0530

    update signup backend

--- public/js/login.js
+++ public/js/login.js
@@ -5,6 +5,10 @@ const signUpBtn = document.getElementById("signUpBtn");
 const loginBtn = document.getElementById("loginBtn");
 const loginEmail = document.getElementById("loginEmail");
 const loginPassword = document.getElementById("loginPassword");
+const username=document.getElementById("name");
+const email=document.getElementById("email");
+const password=document.getElementById("password");
+const number=document.getElementById("number");
 
 signUp.addEventListener("click", () => {
   container.classList.add("right-panel-active");
@@ -37,4 +41,39 @@ function login() {
     });
 }
 
-loginBtn.addEventListener("click", login);
+function signup(){
+  if(number.value=="" ||username.value=="" ||email.value=="" ||password.value==""){
+       alert('please fill the details!!')
+  }
+else{
+  const signupDetails = {
+      name: username.value,
+      email: email.value,
+      number:number.value,
+      password:password.value
+    };
+    axios
+    .post("http://localhost:4000/user/signup", signupDetails)
+    .then((result) => {
+      alert(result.data.message);
+    })
+    .catch((error) => {
+      if (error.response) {
+        const errorMessage = error.response.data.message;
+        alert(errorMessage);
+      } else {
+        alert("An error occurred. Please try again later.");
+      }
+    });
+  }
+
+}
+
+loginBtn.addEventListener("click", async (e)=>{
+  e.preventDefault();
+  await login()
+});
+signUpBtn.addEventListener("click",async(e)=>{
+  e.preventDefault();
+  await signup();
+})
